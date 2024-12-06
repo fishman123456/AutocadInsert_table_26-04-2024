441ca174c777265866984da6e42dee118f90feba
Denis Borisovich Fishman <106389581+fishman123456@users.noreply.github.com>
2024-12-06 10:34:08 +0700

    работает

--- Data.cs
+++ Data.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace AutocadInsert_table_26_04_2024
 {
     public class Data
     {
         public string Poz { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } 
         public string Num { get; set; }
         public string Description { get; set; }
 
@@ -29,6 +30,10 @@ namespace AutocadInsert_table_26_04_2024
             string patternNstr = "\n";
             string patternTab = ";";
             string[] strings = Regex.Split(str, patternNstr);
+            try
+            {
+
+           
             foreach (string s in strings)
             {
                     string[] stringData = Regex.Split(s, patternTab);
@@ -38,6 +43,12 @@ namespace AutocadInsert_table_26_04_2024
                     data.Num = stringData[2];
                     data.Description = stringData[3];
                     listData.Add(data);  
+            }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Не хватает значений в строке" + "\n" + ex.ToString());
+                //throw new Exception("Не хватает значений в строке");
             }
             return listData;
         }
